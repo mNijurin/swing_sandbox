6a2b17a399e70e0f890b39b21976e81b55871da1
Maxim <trickster77777@gmail.com>
2012-10-10 13:21:56 +0600

    * add column to the jtable logic created

--- src/main/java/com/alcodev/sandbox/forms/jtablecustomcreate/JTableCustomCreate.java
+++ src/main/java/com/alcodev/sandbox/forms/jtablecustomcreate/JTableCustomCreate.java
@@ -2,6 +2,7 @@ package com.alcodev.sandbox.forms.jtablecustomcreate;
 
 import ca.odell.glazedlists.BasicEventList;
 import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.gui.TableFormat;
 import ca.odell.glazedlists.impl.ThreadSafeList;
 import ca.odell.glazedlists.swing.EventTableModel;
 import com.jgoodies.forms.builder.PanelBuilder;
@@ -9,7 +10,11 @@ import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Random;
 
 /**
@@ -27,21 +32,26 @@ public class JTableCustomCreate {
 
         EventTableModel<String> model = new EventTableModel<String>(string, new MyTableFormat());
 
-
-//        contentPanel = new JPanel();
-
-        JTable jTable = new JTable();
+        final JTable jTable = new JTable();
         jTable.setModel(model);
+        jTable.setDefaultRenderer(Object.class, new MyRenderer());
 
         JScrollPane scrollPane = new JScrollPane(jTable);
         scrollPane.setPreferredSize(new Dimension(450, 32000));
 
-//        contentPanel.add(scrollPane);
         fillStrings(string);
+
+        JButton jButton = new JButton("test add column");
+        jButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                jTable.getColumnModel().addColumn(new TableColumn(jTable.getColumnCount()));
+            }
+        });
 
         FormLayout layout = new FormLayout(
                 "3dlu, left:p:grow, 3dlu,",
-                "3dlu, top:100dlu:grow, 3dlu,");
+                "3dlu, top:100dlu:grow, 3dlu, top:30dlu:g");
 
         PanelBuilder builder = new PanelBuilder(layout);
         builder.setDefaultDialogBorder();
@@ -49,6 +59,7 @@ public class JTableCustomCreate {
         CellConstraints constraints = new CellConstraints();
 
         builder.add(scrollPane, constraints.xy(2, 2));
+        builder.add(jButton, constraints.xy(2, 4));
 
         contentPanel = builder.getPanel();
     }
@@ -62,5 +73,45 @@ public class JTableCustomCreate {
 
     public JPanel getContentPanel() {
         return contentPanel;
+    }
+
+    public class MyTableFormat implements TableFormat<String> {
+        @Override
+        public int getColumnCount() {
+            return 1;
+        }
+
+        @Override
+        public String getColumnName(int i) {
+            return "asdfadsf";
+        }
+
+        @Override
+        public Object getColumnValue(String s, int index) {
+            if (index == 0) {
+                return s;
+            }
+            if (index == 1) return "1";
+            if (index == 2) return "2";
+            if (index == 3) return "3";
+            return "4";
+        }
+    }
+
+    private class MyRenderer extends DefaultTableCellRenderer {
+        @Override
+        public Component getTableCellRendererComponent(JTable jTable, Object o,
+                                                       boolean isSelected, boolean hasFocus, int row, int column) {
+            JLabel parent = (JLabel) super.getTableCellRendererComponent(jTable, o, isSelected, hasFocus, row, column);
+
+            if (column == 0) {
+                parent.setFont(parent.getFont().deriveFont(Font.BOLD));
+            }
+            int i = row % 2;
+            if (i == 0) {
+                parent.setBackground(Color.decode("#EBFFE9"));
+            } else parent.setBackground(Color.white);
+            return parent;
+        }
     }
 }
