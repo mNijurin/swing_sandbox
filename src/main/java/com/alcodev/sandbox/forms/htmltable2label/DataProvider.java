2222d03f78ce9442e03a1f1869941a2db3deacac
Maxim <trickster77777@gmail.com>
2012-10-19 10:04:39 +0600

    * html div tags example 2 JEditPane sandbox added

--- src/main/java/com/alcodev/sandbox/forms/htmltable2label/DataProvider.java
+++ src/main/java/com/alcodev/sandbox/forms/htmltable2label/DataProvider.java
@@ -9,35 +9,41 @@ public class DataProvider {
     public String provideHtmlTable() {
         String data = "";
 
-        data += "<html>";
+        data += "<html>" +
+                "<style>" +
+                ".abc {color: black; font-size: 14px; font-face: bold; font-family: Arial}" +
+                ".number {color: red; font-size: 20px; font-face: bold; font-family: Arial}" +
+                "</style>" +
+                "<div class='abc'>Title1</div>" +
+                "<div><span class='number'> 146 </span><span>England 1:0 France</span></div>";
 
-        data += "<table width=\"100%\" bgcolor=\"white\">";
-
-        data += "<tr>";
-        data += "<td colspan=\"4\" bgcolor=\"red\">";
-        data += "<font size=+2>" + "Тестовый заголовок" + "</font>";
-        data += "</td>";
-        data += "</tr>";
-
-        data += "<tr>";
-        data += "<td>";
-        data += "110";
-        data += "</td>";
-        data += "<td>";
-        data += "10.12 10:12";
-        data += "</td>";
-        data += "<td>";
-        data += "команда 1 - команда 2";
-        data += "</td>";
-        data += "<td>";
-        data += "аргентина ямайка 5-0";
-        data += "</td>";
-        data += "</tr>";
-
-        data += "</table>";
+//        data += "<table width=\"100%\" bgcolor=\"white\">";
+//
+//        data += "<tr>";
+//        data += "<td colspan=\"4\" bgcolor=\"red\">";
+//        data += "<font size=+2>" + "Тестовый заголовок" + "</font>";
+//        data += "</td>";
+//        data += "</tr>";
+//
+//        data += "<tr>";
+//        data += "<td>";
+//        data += "110";
+//        data += "</td>";
+//        data += "<td>";
+//        data += "10.12 10:12";
+//        data += "</td>";
+//        data += "<td>";
+//        data += "команда 1 - команда 2";
+//        data += "</td>";
+//        data += "<td>";
+//        data += "аргентина ямайка 5-0";
+//        data += "</td>";
+//        data += "</tr>";
+//
+//        data += "</table>";
 
         data += "</html>";
-
+// что б использовать cellpadding , надо значение без ковычек ставить!!!
         return data;
     }
 }
